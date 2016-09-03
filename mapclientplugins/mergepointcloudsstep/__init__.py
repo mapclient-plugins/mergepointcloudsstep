61e0637446cc2fdd5e9603c0d322b3c7a7c5548e
Ju Zhang <jzha263@aucklanduni.ac.nz>
2016-09-03 16:15:29 +1200

    v0.1.0 release

--- mapclientplugins/mergepointcloudsstep/__init__.py
+++ mapclientplugins/mergepointcloudsstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Merge Point Clouds'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mergepointcloudsstep/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.mergepointcloudsstep import step
--- setup.py
+++ setup.py
@@ -4,22 +4,30 @@ import sys, os, io
 # List all of your Python package dependencies in the
 # requirements.txt file
 
-def readfile(filename):
+def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
-        return stream.read().split("\n")
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
 
-readme = readfile("README.rst")[3:]  # skip title
-requires = readfile("requirements.txt")
+readme = readfile("README.rst", split=True)[3:]  # skip title
+requires = readfile("requirements.txt", split=True)
 license = readfile("LICENSE")
 
 setup(name=u'mapclientplugins.mergepointcloudsstep',
-      version='0.0.0',
+      version='0.1.0',
       description='',
-      long_description=readme + license,
+      long_description='
+'.join(readme) + license,
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+      "License :: OSI Approved :: Apache Software License",
+      "Programming Language :: Python",
+    ],
       classifiers=[],
       author=u'Ju Zhang',
       author_email='',
-      url='',
+      url='https://github.com/mapclient-plugins/mergepointcloudsstep',
       license='APACHE',
       packages=find_packages(exclude=['ez_setup',]),
       namespace_packages=['mapclientplugins'],
