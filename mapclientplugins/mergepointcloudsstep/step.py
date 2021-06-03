8e5406fb0785a6e9a4e151816441815ee957ed56
TimmyNic <tim.nicolas@outlook.com>
2021-06-04 10:51:40 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/mergepointcloudsstep/step.py
+++ mapclientplugins/mergepointcloudsstep/step.py
@@ -75,7 +75,7 @@ class MergePointCloudsStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
@@ -114,7 +114,7 @@ class MergePointCloudsStep(WorkflowStepMountPoint):
         '''
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog()
+        d = ConfigureDialog(self._main_window)
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
