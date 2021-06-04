2aa3051afc02dde8d3e398b5a9c7cf17b6145816
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-04 14:10:23 +1200

    Update step.py

--- mapclientplugins/mergepointcloudsstep/step.py
+++ mapclientplugins/mergepointcloudsstep/step.py
@@ -113,7 +113,7 @@ class MergePointCloudsStep(WorkflowStepMountPoint):
         '''
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog(self._main_window)
+        d = ConfigureDialog()
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
