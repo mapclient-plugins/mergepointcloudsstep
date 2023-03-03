c764dc3a391af2d208a99a1293eaf89d08a73c50
TimmyNic <tim.nicolas@outlook.com>
2023-03-03 14:10:50 +1300

    Address PEP8 warnings
    Address as many PEP8 warnings as possible.

--- mapclientplugins/mergepointcloudsstep/step.py
+++ mapclientplugins/mergepointcloudsstep/step.py
@@ -1,6 +1,6 @@
-'''
+"""
 MAP Client Plugin Step
-'''
+"""
 import json
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
@@ -10,10 +10,10 @@ import numpy as np
 
 
 class MergePointCloudsStep(WorkflowStepMountPoint):
-    '''
+    """
     Skeleton step which is intended to be a helpful starting point
     for new steps.
-    '''
+    """
 
     def __init__(self, location):
         super(MergePointCloudsStep, self).__init__('Merge Point Clouds', location)
@@ -35,46 +35,47 @@ class MergePointCloudsStep(WorkflowStepMountPoint):
         self._portData1 = None  # http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud
         self._portData2 = None  # http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud
         # Config:
-        self._config = {}
-        self._config['identifier'] = ''
+        self._config = {
+            'identifier': ''
+        }
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         self._portData2 = np.vstack([self._portData0, self._portData1])
         self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
+        """
         Add your code here that will set the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         uses port for this step then the index can be ignored.
-        '''
+        """
         if index == 0:
             self._portData0 = np.array(dataIn)  # http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud
         elif index == 1:
             self._portData1 = np.array(dataIn)  # http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud
 
     def getPortData(self, index):
-        '''
+        """
         Add your code here that will return the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         provides port for this step then the index can be ignored.
-        '''
+        """
         return self._portData2  # http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
+        """
         dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
@@ -88,29 +89,29 @@ class MergePointCloudsStep(WorkflowStepMountPoint):
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to string.  This method should
         implement the opposite of 'deserialize'.
-        '''
+        """
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from string.  This method should
         implement the opposite of 'serialize'.
-        '''
+        """
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
